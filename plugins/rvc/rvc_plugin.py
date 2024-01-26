a64667c347d0eb71ef71c522e7e294ab2316168b
Xiaohei <15852786007ysc@gmail.com>
2024-01-26 00:53:39 -0500

    rvc model selection

--- plugins/rvc/rvc_plugin.py
+++ plugins/rvc/rvc_plugin.py
@@ -22,6 +22,7 @@ class RVCPlugin(TTSPluginInterface):
     rvc_index_dir = os.path.join(current_module_directory, "rvc_index_dir")
 
     edge_tts_voice = "en-US-AnaNeural"
+    rvc_model_name = 'kikuri.pth'
 
     def init(self):
         # where model.pth files are stored.
@@ -33,10 +34,10 @@ class RVCPlugin(TTSPluginInterface):
         # If the output audio tensor should block until fully loaded, this can be ignored. But if you want to run in a larger torch pipeline, setting to False will improve performance a little.
         os.environ['RVC_RETURNBLOCKING'] = 'True'
 
-        self.model = RVC(
-            'kikuri.pth', index='added_IVF571_Flat_nprobe_1_kikuri_v2')
+        self.model = RVC(self.rvc_model_name)
         print(self.model.name)
         print('Paths', self.model.model_path, self.model.index_path)
+        self.update_rvc_model_list()
 
     def synthesize(self, text):
 
@@ -54,7 +55,7 @@ class RVCPlugin(TTSPluginInterface):
         samples = np.array(audio.get_array_of_samples())
 
         aud, sr = load_torchaudio(wav_filename)
-        paudio1 = self.model(aud, f0_up_key=6, output_device='cpu',
+        paudio1 = self.model(aud, f0_up_key=0, output_device='cpu',
                              output_volume=RVC.MATCH_ORIGINAL, index_rate=.75)
 
         sf.write(self.RVC_OUTPUT_FILENAME, paudio1, 44100)
@@ -73,11 +74,14 @@ class RVCPlugin(TTSPluginInterface):
                     value=self.edge_tts_voice,
                     label="edge_tts_speaker: "
                 )
-                # self.rvc_model = gr.Dropdown(
-                #     choices=[],
-                #     value=self.selected_style['name'],
-                #     label="rvc_model: "
-                # )
+                self.rvc_model_dropdown = gr.Dropdown(label="RVC models:",
+                                                      choices=self.rvc_model_names, value=self.rvc_model_names[0] if len(self.rvc_model_names) > 0 else None, interactive=True)
+                self.refresh_button = gr.Button("Refresh", variant="primary")
+
+                self.rvc_model_dropdown.input(self.on_rvc_model_change, inputs=[
+                    self.rvc_model_dropdown], outputs=[])
+                self.refresh_button.click(
+                    self.on_refresh, outputs=[self.rvc_model_dropdown])
 
                 self.edge_tts_speaker_dropdown.input(self.on_speaker_change, inputs=[
                     self.edge_tts_speaker_dropdown], outputs=[])
@@ -88,3 +92,18 @@ class RVCPlugin(TTSPluginInterface):
 
     def on_speaker_change(self, choice):
         self.edge_tts_voice = choice
+
+    def on_rvc_model_change(self, choice):
+        self.rvc_model_name = choice
+        self.model = RVC(self.rvc_model_name)
+
+    def on_refresh(self):
+        self.update_rvc_model_list()
+        return gr.update(choices=self.rvc_model_names)
+
+    def update_rvc_model_list(self):
+        self.rvc_model_names = []
+        for name in os.listdir(self.rvc_model_dir):
+            if name.endswith(".pth"):
+                self.rvc_model_names.append(name)
+                print(self.rvc_model_names)
