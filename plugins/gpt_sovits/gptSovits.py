aadf8ee6209e1c459750ffcd79cf436740ffb885
Xiaohei <15852786007ysc@gmail.com>
2024-09-13 21:12:54 -0400

    add gpt sovits direct

--- plugins/gpt_sovits/gptSovits.py
+++ plugins/gpt_sovits/gptSovits.py
@@ -1,75 +1,59 @@
-import requests
+
+
 import os
-import gradio as gr
+import re
+import sys
 from pluginInterface import TTSPluginInterface
-import subprocess
+import gradio as gr
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "GPT_SoVITS"))
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+import GPT_SoVITS.api_direct as api
 
+class GPTSOVITS_Plugin(TTSPluginInterface):
 
-class gptSovits(TTSPluginInterface):
     current_module_directory = os.path.dirname(__file__)
-    output_dir = os.path.join(current_module_directory, "output.wav")
-    server_dir = os.path.join(current_module_directory, "GPT_SoVITS")
-    lang = ["zh","en","ja"]
-    url = "http://127.0.0.1:9880"
+    OUTPUT_FILENAME = os.path.join(
+        current_module_directory, "output.wav")
+   
 
     def init(self):
-        self.language = self.lang[1]
-        command = [
-        "python", "api.py",
-        "-s", r"./SoVITS_weights/kokomi2_e15_s2295.pth",
-        "-g", r"./GPT_weights/kokomi2-e10.ckpt",
-        "-dr", r"./ふーん、新作対戦ゲーム設置しました、か.mp3",
-        "-dt", "ふーん、新作対戦ゲーム設置しました、か",
-        "-dl", "ja"
-        ]
-
-        # command = [
-        # "python", "api.py",
-        # "-s", r"./SoVITS_weights/kokomi2_e15_s2295.pth",
-        # "-g", r"./GPT_weights/kokomi2-e10.ckpt",
-        # "-dr", r"./vo_card_kokomi_freetalk_01.wav",
-        # "-dt", "The situation is ever-changing in a card game. To emerge victorious, you have to be willing to take some risks.",
-        # "-dl", "en"
-        # ]
-
-        # command = [
-        # "python", "api.py",
-        # "-s", r"./SoVITS_weights/nene60_test_e8_s280.pth",
-        # "-g", r"./GPT_weights/nene60-test-e20.ckpt",
-        # "-dr", r"./sample.mp3",
-        # "-dt", "ふーん、新作対戦ゲーム設置しました、か",
-        # "-dl", "ja"
-        # ]
-        print(f"process = subprocess.Popen({command}, cwd={self.server_dir}, shell=True)")
-        process = subprocess.Popen(command, cwd=self.server_dir, shell=True)
-        print(f"GPT-Sovits Server started with PID {process.pid}")
+        api.init()
 
     def synthesize(self, text):
-        params = {
-            'text': text,
-            'text_language': self.language
-        }
-        response = requests.get(self.url, params=params)
+        text = self.preprocess_text(text)
+        response = api.tts_endpoint(text=text, text_language='auto')
 
-        if response.status_code == 200:
-            with open(self.output_dir, 'wb') as file:
-                file.write(response.content)
-            print("Audio file downloaded successfully:", self.output_dir)
-        else:
-            print("Failed to download audio file. Status Code:", response.status_code)
-            
-        return response.content
+        try:
+            with open(self.OUTPUT_FILENAME, 'wb') as file:
+                file.write(response)
+        except Exception as e:
+            print("Failed to generate audio file.", e)
 
+        return response
+
+
+       
 
     def create_ui(self):
-        with gr.Accordion(label="gpt-sovits Options", open=False):
+        with gr.Accordion(label="Gpt-sovits Options", open=False):
             with gr.Row():
-                self.language_dropdown = gr.Dropdown(choices=self.lang, value=self.language, label='language')
-                
-        self.language_dropdown.change(self.update_language, inputs=[self.language_dropdown])
-    
-    def update_language(self, input):
-        self.language = input
-    
-    
+                # self.rvc_model_dropdown = gr.Dropdown(label="RVC models:",
+                #                                         choices=self.rvc_model_names, value=self.rvc_model_name if len(self.rvc_model_names) > 0 else None, interactive=True)
+                self.refresh_button = gr.Button("Refresh", variant="primary")
+
+    def preprocess_text(self, text):
+        print(f"replacing decimal point with the word point.")
+        print(f"original:) {text}")
+
+        pattern = r'\b\d*\.\d+\b'
+
+        def replace_match(match):
+            decimal_number = match.group(0)
+            return decimal_number.replace('.', ' point ')
+
+        # Replace all occurrences of decimal patterns in the text
+        replaced_text = re.sub(pattern, replace_match, text)
+        print(f"replaced_text: {replaced_text}")
+        
+        return replaced_text
